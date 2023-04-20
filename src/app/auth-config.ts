1ea2d810ff4e66c52628a4b5aaf877ffddf6020b
ThongSophal <61540557+ThongSophal@users.noreply.github.com>
2023-04-20 07:29:48 +0700

    Update auth-config.ts

--- src/app/auth-config.ts
+++ src/app/auth-config.ts
@@ -3,8 +3,7 @@ import { AuthConfig } from "angular-oauth2-oidc";
 export const authConfig: AuthConfig = {
   issuer: 'https://accountuat.nssf.gov.kh',
   redirectUri: 'http://localhost:4200/',
- // clientId: '2ADA5F1E-357D-401B-92A3-F817C567FEDFO',
- clientId:'2ADA5F1E-357D-401B-92A3-F817C567FEDF-FO',
+  clientId: '2ADA5F1E-357D-401B-92A3-F817C567FEDFO',
   scope: 'openid profile offline_access beneficiary_registration_api registration_api webadmin_api enterprise roles IDCard',
   clearHashAfterLogin: false,
   strictDiscoveryDocumentValidation: false,
